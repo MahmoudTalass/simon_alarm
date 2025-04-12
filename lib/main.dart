115dbd75eb6371f75ebaa3c76f202f55c7a24108
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2025-04-12 16:04:44 -0400

    Resolve issue with conflicting main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:simon_alarm/alarms_page.dart';
 
-<<<<<<< HEAD
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 2, 39, 68),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 75),
-            Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black, // Added opacity for better look
-                  blurRadius: 8,
-                  spreadRadius: 2,
-                ),
-              ],
-            ),
-            child: const Center(child: Text('Current Alarm\n 00:00 AM/PM')), // This is the Container's child
-          ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-=======
 void main() {
-  runApp(
-    MaterialApp(
-      home: MainNavigation(),
-      // initialRoute: '/',
-      // routes: {
-      //   '/': (context) => AlarmsPage(),
-      //   // '/countdown': (context) => CountdownScreen(),
-      //   //'/game': (context) => SimonGameScreen(),
-      //   //'/success': (context) => SuccessScreen(),
-      // },
-    ),
-  );
+  runApp(MaterialApp(home: MainNavigation()));
 }
 
 class MainNavigation extends StatefulWidget {
@@ -87,4 +36,3 @@ class _MainNavigationState extends State<MainNavigation> {
     );
   }
 }
->>>>>>> be95370 (Update navigation mechanism)
